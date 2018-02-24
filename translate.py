b1e66ac9efd260732aedd81aa8923e400fe2dd32
Fenrir2908 <archurilov@yandex.ru>
2018-02-24 13:56:16 +0300

    Готово

--- translate.py
+++ translate.py
@@ -6,7 +6,6 @@ TranslateURL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 DetectURL = 'https://translate.yandex.net/api/v1.5/tr.json/detect'
 GetLangURL = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs'
 
-cut_text = ''
 origin_text = ''
 trans_file_directory = ''
 
@@ -16,7 +15,7 @@ with open('e:/x/config/config.json') as config_file:
     settings = json.load(config_file)
 
 def user_messege():
-    print('welcome motherfucker!\n'
+    print('Добро пожаловать!\n'
         'Это переводчик с русского на английский\n'
         'Для начала перевода введи "p"\n'
         'Для определения языка нажмите "d"\n'
@@ -24,53 +23,35 @@ def user_messege():
         'Для выхода нажми "e"\n')
 
 
-def translating_file():
-    some_text = []
-
-    usr_input = input('Укажите путь к файлу: \n')
-
-    with open(usr_input) as user_file:
-        for line in user_file:
-            some_text.append(line)
-
-    return some_text[0]
-
-
-def translated_file():
-    usr_input = input('Укажите директорию и имя для сохранения файла: \n')
-
-    return usr_input
-
-
-def translate(path):
-    with open(path) as origin_text:
-
+def list_on_string():
+    with open(settings['open_path']) as origin_text:
         s_text = []
 
         for x in origin_text:
-            x.replace('\n', '---')
+            x = x.replace('\n', '')
             s_text.append(x)
 
-        #print(s_text)
+        s_text = [value for value in s_text if value]
+        my_srting = ''.join(s_text)
+        return my_srting
+
+
+def translate():
+
+        my_string = list_on_string()
 
         params = {
             'key': KEY,
-            'text': s_text,
+            'text': my_string,
             'lang': settings['translation'],
         }
         respounce = requests.get(TranslateURL, params=params)
 
         text = respounce.json()['text'][0]
-
         return text
 
 
-def detectlang(cut_textline):
-    if cut_textline != '':
-        text = cut_textline
-    else:
-        text = input('Введите текст на исходном языке: \n')
-
+def get_langcode(text):
     params = {
         'key': KEY,
         'text': text,
@@ -79,6 +60,10 @@ def detectlang(cut_textline):
     respounce = requests.get(DetectURL, params=params)
     langcode = respounce.json()['lang']
 
+    return langcode
+
+
+def get_lang(langcode):
     params = {
         'key': KEY,
         'ui': langcode,
@@ -87,31 +72,55 @@ def detectlang(cut_textline):
     respounce = requests.get(GetLangURL, params=params)
     answer = respounce.json()['langs'][langcode]
 
-    print('Язык текста: %s\n' % answer)
+    return answer
+
+
+def detect_sub_menu():
+    user_input = input('Нажмите "w" и введите текст вручную, или нажмите "f", '
+                       'чтобы получить текст из указанного файла\n')
+    if user_input == 'w':
+        user_input = input('Введите текст: \n')
+        return user_input
+    if user_input == 'f':
+        my_string = list_on_string()
+        return my_string
+
+
+def detectlang():
+    text = detect_sub_menu()
+
+    langcode = get_langcode(text)
+    lang = get_lang(langcode)
+
+    print('Язык текста: %s\n' % lang)
+
+
+def saving_file(setting, key):
+    question = input('Сохранить настройки? y/n \n')
+    if question == 'y':
+        if key == 'i':
+            settings['open_path'] = setting
+        if key == 'o':
+            settings['save_path'] = setting
+        if key == 't':
+            settings['translation'] = setting
+
+        with open('e:/x/config/config.json', 'w') as config_file:
+            json.dump(settings, config_file)
+    if question == 'n':
+        menu_settings()
 
 
 def menu_settings_action(key_value):
     if key_value == 'i':
         input_setting = input('Укажи новый путь к файлу для перевода: \n')
-        question = input('Сохранить настройки? y/n \n')
-        if question == 'y':
-            settings['open_path'] = input_setting
-            with open('e:/x/config/config.json', 'w') as config_file:
-                json.dump(settings, config_file)
+        saving_file(input_setting, 'i')
     if key_value == 'o':
         input_setting = input('Укажи новый путь к файлу для сохранения: \n')
-        question = input('Сохранить настройки? y/n \n')
-        if question == 'y':
-            settings['save_path'] = input_setting
-            with open('e:/x/config/config.json', 'w') as config_file:
-                json.dump(settings, config_file)
+        saving_file(input_setting, 'o')
     if key_value == 't':
         input_setting = input('Укажи новое направление перевода (в формате ru-en): \n')
-        question = input('Сохранить настройки? y/n \n')
-        if question == 'y':
-            settings['translation'] = input_setting
-            with open('e:/x/config/config.json', 'w') as config_file:
-                json.dump(settings, config_file)
+        saving_file(input_setting, 't')
 
 
 def menu_settings():
@@ -137,24 +146,22 @@ def menu_settings():
         if user_input == 'm':
             break
 
-def write_text_on_file(file_directory, ready_text):
-    with open(file_directory, 'w') as file_for_saving:
+
+def write_text_on_file(ready_text):
+    with open(settings['save_path'], 'w') as file_for_saving:
         file_for_saving.write(ready_text)
+    print('Готово!\n')
 
 
 while True:
     user_messege()
     user_input = input()
 
-    #if user_input == 'i':
-    #    origin_text = translating_file()
-    #if user_input == 'o':
-    #    trans_file_directory = translated_file()
     if user_input == 'p':
-        ready_text = translate(settings['open_path'])
-        write_text_on_file(settings['save_path'], ready_text)
+        ready_text = translate()
+        write_text_on_file(ready_text)
     if user_input == 'd':
-        detectlang(cut_text)
+        detectlang()
     if user_input == 's':
         menu_settings()
     if user_input == 'e':
